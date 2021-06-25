e8a30008cfe1ec01bb6ffeb83963abb365b49e21
PujanGupta <onlinelearning@outlook.sg>
2021-06-25 10:30:29 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from collections import Counter
+while True:
+    userInput = input("Enter something: ")
+    print(
+        Counter(
+            userInput
+        )
+    )
+    
+#This program is not ment for any production use it is just ment for syntax reference.
+    
+    
